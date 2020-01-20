2382be2fd4b5ede052f6987c8317763aded929a9
Cam Prier <CSPrier@gmail.com>
2020-01-20 14:39:09 -0500

    Added a console log to on connect in init

--- src/sockets.js
+++ src/sockets.js
@@ -3,7 +3,7 @@ const socketIO = require('socket.io');
 function init(server) {
   const io = socketIO(server);
   io.on('connection', (socket) => {
-    console.log('A user connected!');
+    console.log(`A user connected to socket: ${socket}!`);
   });
 };
 
